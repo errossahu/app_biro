3ddf49b217cec3ececd5269f26060c2d9d1c0d84
Yohanes Erros Sahu <duragonprojek@gmail.com>
2021-03-11 17:57:15 +0800

    Second Commit

---
+++ src/components/Slide.vue
@@ -0,0 +1,39 @@
+<template>
+  <splide :options="options">
+    <splide-slide>
+      <img src="../assets/logo.png" />
+    </splide-slide>
+    <splide-slide>
+      <img src="../assets/logo.png" />
+    </splide-slide>
+    <splide-slide>
+      <img src="../assets/logo.png" />
+    </splide-slide>
+  </splide>
+</template>
+
+<script>
+import "@splidejs/splide/dist/css/themes/splide-skyblue.min.css";
+export default {
+  name: "Slide",
+  data() {
+    return {
+      options: {
+        tpye: "loop",
+        height: "9rem",
+        autoWidth: true,
+        focus: "center",
+        padding: {
+          right: "5rem",
+          left: "5rem",
+        },
+      },
+    };
+  },
+};
+</script>
+
+
+
+<style>
+</style>
